a72b49890f5b813b46b57c9351b0e8a7c0a2b71e
Nikita <nikita_bulak@mail.ru>
2024-06-09 22:06:41 +0300

    initial commit

---
+++ src/main/java/ru/bulak/loggingstartergradle/LoggingConfiguration.java
@@ -0,0 +1,7 @@
+package ru.bulak.loggingstartergradle;
+
+public class LoggingConfiguration {
+    public static void testLog(String forPrinting){
+        System.out.println(forPrinting);
+    }
+}
